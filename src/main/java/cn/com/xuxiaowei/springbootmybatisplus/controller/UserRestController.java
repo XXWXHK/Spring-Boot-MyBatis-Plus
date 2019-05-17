274c123429651d3bb73266bf6d880b9442b19276
徐晓伟 <xuxiaowei@live.hk>
2019-05-17 21:11:50 +0800

    根据 ID 查询

--- src/main/java/cn/com/xuxiaowei/springbootmybatisplus/controller/UserRestController.java
+++ src/main/java/cn/com/xuxiaowei/springbootmybatisplus/controller/UserRestController.java
@@ -1,9 +1,16 @@
 package cn.com.xuxiaowei.springbootmybatisplus.controller;
 
 
+import cn.com.xuxiaowei.springbootmybatisplus.entity.User;
+import cn.com.xuxiaowei.springbootmybatisplus.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,6 +23,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserRestController {
+
+    /**
+     * 用户表（测试） 服务类
+     */
+    private final IUserService userService;
+
+    /**
+     * 尽量使用这种方式注入（最好的方式）
+     *
+     * @param userService 用户表（测试） 服务类
+     */
+    @Autowired
+    public UserRestController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * 根据 ID 查询
+     *
+     * @param id 用户主键
+     */
+    @RequestMapping("/getById")
+    public Map<String, Object> getById(HttpServletRequest request, HttpServletResponse response, String id) {
+
+        Map<String, Object> map = new HashMap<>(4);
+
+        User user = userService.getById(id);
+
+        map.put("user", user);
+
+        return map;
+    }
 
 }
 
