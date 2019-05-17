06372976e4ceaaf3352117736b5710e62cef5522
徐晓伟 <xuxiaowei@live.hk>
2019-05-17 21:01:23 +0800

    测试

--- src/test/java/cn/com/xuxiaowei/springbootmybatisplus/SpringBootMybatisPlusApplicationTests.java
+++ src/test/java/cn/com/xuxiaowei/springbootmybatisplus/SpringBootMybatisPlusApplicationTests.java
@@ -1,16 +1,32 @@
 package cn.com.xuxiaowei.springbootmybatisplus;
 
+import cn.com.xuxiaowei.springbootmybatisplus.entity.User;
+import cn.com.xuxiaowei.springbootmybatisplus.service.IUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Random;
+import java.util.UUID;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootMybatisPlusApplicationTests {
 
+    @Autowired
+    private IUserService userService;
+
     @Test
     public void contextLoads() {
+
+        getById();
+    }
+
+    void getById() {
+        User user = userService.getById(1);
+        System.err.println(user);
     }
 
 }
